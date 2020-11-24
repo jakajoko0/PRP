095f787f3527e0fbd24ee2300324f306858e869a
PBSITProjects <dgrenier@smallbizpros.com>
2020-11-24 14:15:38 -0500

    Check if we can rescue errors in mailer

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,18 @@
 class ApplicationMailer < ActionMailer::Base
   default from: 'PR+P USA <padgettportal@smallbizpros.com>'
   layout 'mailer'
+
+  ERRORS_TO_RESCUE = [
+  	Net::SMTPAuthenticationError,
+    Net::SMTPServerBusy,
+    Net::SMTPSyntaxError,
+    Net::SMTPFatalError,
+    Net::SMTPUnknownError,
+    Errno::ECONNREFUSED
+  ]
+
+  rescue_from *ERRORS_TO_RESCUE  do |exception|
+  	redirect_to root_path, notice: "An error occured while trying to send email notification"
+  end
+
 end
